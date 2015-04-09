b7b23f9840af377f37617f3bbb79556342d74133
Ilija Hadzic <ilijahadzic@gmail.com>
2015-04-09 08:59:23 -0400

    main: replace prints with calls to logger
    Initialize the logger early in the process and
    use log.X functions instead of print functions.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- __main__.py
+++ __main__.py
@@ -1,23 +1,26 @@
 #/usr/bin/env python2
 
 import web
+import log
 import ConfigParser
 
+_log = log.TrivialLogger()
 _config_file_list = ['./jim.cfg', '/etc/jim.cfg']
 _config_file_parser = ConfigParser.RawConfigParser()
 _config_ok = True
 try:
     _config_file_list = _config_file_parser.read(_config_file_list)
 except:
-    print("cannot parse configuration file(s)")
+    _log.error("cannot parse configuration file(s)")
     _config_ok = False
 if len(_config_file_list) == 0:
-    print("no configuration file found")
+    _log.error("no configuration file found")
     _config_ok = False
 else:
-    print("using configuration file {}".format(_config_file_list[0]))
+    _log.info("using configuration file {}".format(_config_file_list[0]))
+
 
 if _config_ok:
-    print("starting server")
+    _log.info("starting server")
     web.run_server()
-    print("server exited")
+    _log.info("server exited")
