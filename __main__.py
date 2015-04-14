80593538b57eed5eb23db13d74d71ddecd498446
Ilija Hadzic <ilijahadzic@gmail.com>
2015-04-14 19:05:14 -0400

    main: add support for configuration file
    Look for configuration file in local directory or in etc/
    directory and read it if found. Bail out if there is no configuration
    file available.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- __main__.py
+++ __main__.py
@@ -4,6 +4,23 @@ import web
 import log
 import ConfigParser
 
+def read_config(parser):
+    cfg = {}
+    try:
+        http_port = parser.get("web", "http_port")
+    except:
+        http_port = None
+    try:
+        https_port = parser.get("web", "https_port")
+    except:
+        https_port = None
+    if http_port:
+        cfg['http_port'] = int(http_port)
+    if https_port:
+        cfg['https_port'] = int(https_port)
+    _log.info(cfg)
+    return cfg
+
 _log = log.TrivialLogger()
 _config_file_list = ['./jim.cfg', '/etc/jim.cfg']
 _config_file_parser = ConfigParser.RawConfigParser()
@@ -18,9 +35,11 @@ if len(_config_file_list) == 0:
     _config_ok = False
 else:
     _log.info("using configuration file {}".format(_config_file_list[0]))
-
-
 if _config_ok:
+    _cfg = read_config(_config_file_parser)
+    _log.info("server configuration: {}".format(_cfg))
     _log.info("starting server")
     web.run_server(log_facility = _log)
     _log.info("server exited")
+else:
+    _log.error("configuration error")
