35028b84d4757e1343a97da653670db049ac5e8d
Ilija Hadzic <ilijahadzic@gmail.com>
2015-04-28 19:30:14 -0400

    web: replace default handler with static handler
    There is no need to reinvent the wheel. Just use Tornado's
    built-in static handler.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- web.py
+++ web.py
@@ -2,39 +2,12 @@
 
 import tornado
 import log
-import magic
 from tornado import web, httpserver
 
 _http_server = None
 _https_server = None
 _html_root = './'
 _log = None
-_magic = None
-
-class DefaultHandler(tornado.web.RequestHandler):
-    def get(self, match):
-        _log.info("incoming request: {}".format(self.request))
-        _log.info("matched default match: {}".format(match))
-        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-        self.set_header("Connection", "close")
-        if match:
-            fname = _html_root + '/' + match
-        else:
-            fname = _html_root + '/index.html'
-        _log.info("fname: {}".format(fname))
-        try:
-            with open(fname, 'rb') as fd:
-                content = fd.read()
-            mime_type = _magic.file(fname)
-            self.set_header("Content-type",  mime_type)
-            self.finish(content)
-        except:
-            self.set_status(404)
-            self.finish("Not found: {}".format(match))
-
-_app = tornado.web.Application([
-        ('/(.*)', DefaultHandler)
-])
 
 # TODO: SSL needs this
 # ssl_options['certfile'] - server certificate
@@ -45,25 +18,20 @@ def run_server(ssl_options = {}, http_port = 80, https_port = 443, log_facility 
     global _http_server
     global _https_server
     global _log
-    global _magic
-    global _html_root
 
-    # http://www.zak.co.il/tddpirate/2013/03/03/the-python-module-for-file-type-identification-called-magic-is-not-standardized/
-    try:
-        _magic = magic.open(magic.MAGIC_MIME_TYPE)
-        _magic.load()
-    except AttributeError,e:
-        _magic = magic.Magic(mime=True)
-        _magic.file = _magic.from_file
+    # list handlers for REST calls here
+    handlers = []
 
     if log_facility:
         _log = log_facility
     else:
         _log = log.TrivialLogger()
-    _html_root = html_root
+
+    handlers.append(('/(.*)', web.StaticFileHandler, {'path': html_root}))
+    app = tornado.web.Application(handlers)
     _log.info("creating servers")
-    _http_server = tornado.httpserver.HTTPServer(_app, no_keep_alive = False)
-    _https_server = tornado.httpserver.HTTPServer(_app, no_keep_alive = False, ssl_options = ssl_options)
+    _http_server = tornado.httpserver.HTTPServer(app, no_keep_alive = False)
+    _https_server = tornado.httpserver.HTTPServer(app, no_keep_alive = False, ssl_options = ssl_options)
     _log.info("setting up TCP ports")
     _http_server.listen(http_port)
     _https_server.listen(https_port)
