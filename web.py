64ada9a2608603ecdcfafd1cdb67afa13230bcd3
Ilija Hadzic <ilijahadzic@gmail.com>
2015-05-10 14:37:30 -0400

    web: common initalizer for dynamic handlers
    All dynamic handlers will have set headers to turn off
    caching. Rather than repeating that code in the handlers,
    do it in the initialize method of the base handler.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- web.py
+++ web.py
@@ -14,15 +14,18 @@ _log = None
 # in the dictionary. Normally, we need one to point to the 'CA'
 _test_ssl_options = { 'certfile' : './certs/cert.pem', 'keyfile': './certs/key.pem' }
 
+class DynamicBaseHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.set_header("Pragma", "no-cache")
+        self.set_header("Expires", "0")
+
 class RootHandler(tornado.web.RequestHandler):
     def get(self):
         self.redirect('/index.html', permanent = True)
 
-class DateHandler(tornado.web.RequestHandler):
+class DateHandler(DynamicBaseHandler):
     def get(self):
-        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-        self.set_header("Pragma", "no-cache")
-        self.set_header("Expires", "0")
         self.render('date.html', date_string = str(datetime.datetime.now()))
 
 def run_server(ssl_options = _test_ssl_options, http_port = 80, https_port = 443, log_facility = None, html_root = './html', template_root = './templates'):
