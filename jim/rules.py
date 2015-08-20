a3d0feb482634272b6d8b5dfe40bfdc86fd3305b
Ilija Hadzic <ilijahadzic@gmail.com>
2015-08-20 15:12:37 -0400

    rules: determine the winner from the match score
    Process match scores for each set, make sure that it is
    valid, determine which player won two sets and declare
    that player a winner.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- jim/rules.py
+++ jim/rules.py
@@ -1,3 +1,11 @@
+import logging
+
+_log = logging.getLogger("rules")
+
+def set_results(cgames, ogames):
+    for i in range(0, len(ogames)):
+        yield (cgames[i], ogames[i])
+
 def process_match(cid, oid, cgames, ogames):
     if cid == oid:
         return None, "players cannot play against themselves"
@@ -7,5 +15,19 @@ def process_match(cid, oid, cgames, ogames):
         return None, "cannot play more than three sets"
     if len(cgames) < 2:
         return None, "must play at least two sets"
-    # TODO: really process the match
-    return 42, None
+    cwset = 0
+    owset = 0
+    for s in set_results(cgames, ogames):
+        if s in [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (7, 5), (7, 6)]:
+            cwset = cwset + 1
+        elif s in [(0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 7), (6, 7)]:
+            owset = owset + 1
+        else:
+            return None, "invalid set score: {}".format(s)
+    _log.info("cwset={}, owset={}".format(cwset, owset))
+    if cwset == 2 and owset in [0, 1]:
+        return cid, None
+    elif owset == 2 and cwset in [0, 1]:
+        return oid, None
+    else:
+        return None, "invalid match score: ({}, {})".format(cwset, owset)
