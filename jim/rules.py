8e09db1983b7598ca4c38470b2ee477ec4f18ec0
Ilija Hadzic <ilijahadzic@gmail.com>
2015-08-21 04:29:51 -0400

    rules: add support for forfeited matches
    Enforce the 6-0,6-0 requirement and determine the winner
    by forfeit.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- jim/rules.py
+++ jim/rules.py
@@ -26,7 +26,26 @@ def loser_points(games_won, challenge_points = False):
         points = 5
     return points
 
-def process_match(cid, oid, cgames, ogames, retired = False):
+# Rule 6.4: No Shows - If a player does not show up for a match within 15 minutes of
+# the scheduled start time, the opponent wins the match by forfeit and should
+# report the match as a 6-0, 6-0 victory, with a check mark in the space
+# provided on the match report form to indicate that it was a forfeit. The non-showing
+# person will not be awarded any points but will be charged with one loss.
+# The person who showed up will be awarded 30 points and one win. There will be no
+# between ladder movement as a result of this match report.
+def process_forfeit(cid, oid, cgames, ogames):
+    if len(cgames) != 2:
+        return None, None, None, "forfeited match score must have exactly two sets"
+    elif cgames[0] == 6 and ogames[0] == 0 and cgames[1] == 6 and ogames[1] == 0:
+        # challenger won by forfeit
+        return cid, 30, 0, None
+    elif cgames[0] == 0 and ogames[0] == 6 and cgames[1] == 0 and ogames[1] == 6:
+        # opponent won by forfeit
+        return oid, 0, 30, None
+    else:
+        return None, None, None, "forfeited match score must be 6-0, 6-0"
+
+def process_match(cid, oid, cgames, ogames, retired = False, forfeited = False):
     if cid == oid:
         return None, None, None, "players cannot play against themselves"
     if len(cgames) != len(ogames):
@@ -35,8 +54,12 @@ def process_match(cid, oid, cgames, ogames, retired = False):
         return None, None, None, "cannot play more than three sets"
     if len(cgames) < 2:
         return None, None, None, "must play at least two sets"
+    if forfeited and retired:
+        return None, None, None, "cannot retire and forfeit at the same time"
     cwset = 0
     owset = 0
+    if forfeited:
+        return process_forfeit(cid, oid, cgames, ogames)
     for s in set_results(cgames, ogames):
         if s in [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (7, 5), (7, 6)]:
             if cwset < 2 and owset < 2:
