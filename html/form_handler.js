c799f17dfcd12afbaae80ce4bb6411eed0efd3a6
Ilija Hadzic <ilijahadzic@gmail.com>
2015-10-13 19:29:53 -0400

    html: send the REST call on form submit
    When the form is submitted, intercept the event in the
    JavaScript form handler and send the appropriate REST call
    using XMLHttpRequest object.
    
    Catch the JSON response in response handler function.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- html/form_handler.js
+++ html/form_handler.js
@@ -1,22 +1,39 @@
 function form_to_query(form)
 {
     var i;
+    var first_arg = true;
     var q = form.action;
 
     for (i = 0; i < form.length; i++)
-        if (form[i].value)
-            q += "&" + form[i].name + "=" + form[i].value;
+        if (form[i].value) {
+            if (first_arg) {
+                q += "?";
+                first_arg = false;
+            } else
+                q += "&";
+            q += form[i].name + "=" + form[i].value;
+        }
     return q;
+}
+
+function process_submit_response(response)
+{
+    alert("got this: " + response);
 }
 
 function enter_player()
 {
     var form, query;
+    var xhttp = new XMLHttpRequest();
 
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200)
+            process_submit_response(xhttp.responseText);
+    }
     form = document.getElementById('form_enter_player');
     query = form_to_query(form);
-    // TODO: send the query
-    alert(query);
+    xhttp.open("GET", query, true);
+    xhttp.send();
     // prevent default form submission (we do everything from this script)
     return false;
 }
