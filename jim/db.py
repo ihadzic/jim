300da1d03bb2f731c8fc60dc7f11f7469e597261
Ilija Hadzic <ilijahadzic@gmail.com>
2015-11-25 23:10:47 -0500

    db: add players table to db schema
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- jim/db.py
+++ jim/db.py
@@ -9,8 +9,13 @@ import os
 # be played from the current revision number to the end
 # of the table
 _schema = [
+
     [ 'CREATE TABLE revisions (id INTEGER PRIMARY KEY NOT NULL, date DATE, comment TEXT);',
-      'INSERT INTO revisions (date, comment) VALUES (date("now"), "empty database");' ]
+      'INSERT INTO revisions (date, comment) VALUES (date("now"), "empty database");' ],
+
+    [ 'CREATE TABLE players (id INTEGER PRIMARY KEY  NOT NULL, last_name TEXT, first_name TEXT, username TEXT UNIQUE, password_hash TEXT, password_seed TEXT, cell_phone TEXT, home_phone TEXT, work_phone TEXT, email TEXT, company TEXT, ladder TEXT, active BOOL NOT NULL  DEFAULT false, points INTEGER NOT NULL  DEFAULT 0);',
+      'INSERT INTO revisions (date, comment) VALUES (date("now"), "add players table");']
+
 ]
 
 
