ea29a3091b0a810b0c9247739335339b87c3ca24
Ilija Hadzic <ilijahadzic@gmail.com>
2016-01-23 12:31:34 -0500

    db: recalculate points and match counters on promotion
    When the playe is promoted, the points reset to 30
    and ladder wins/losses reset to 1/0.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- jim/db.py
+++ jim/db.py
@@ -253,7 +253,11 @@ class Database:
         # promotion to higher ladder, if winner came from lower ladder
         if self._compare_ladders(winner_ladder, loser_ladder) < 0:
             winner_ladder = loser_ladder
-        # TODO: recalculate the points and match losses/wins in case of a promotion
+            # if winner is promoted, the points and ladder match counters reset to zero
+            # they will be updated to 30 and 1, respectively in normal calculation
+            self._cursor.execute("UPDATE players SET points=0 WHERE id=?", (winner_id,))
+            self._cursor.execute("UPDATE players SET ladder_wins=0 WHERE id=?", (winner_id,))
+            self._cursor.execute("UPDATE players SET ladder_losses=0 WHERE id=?", (winner_id,))
         self._log.debug("add_match: fields are {}".format(fields_tuple))
         self._log.debug("add_match: values are {}".format(values_tuple))
         self._cursor.execute("UPDATE players SET points=points+? WHERE id=?",
