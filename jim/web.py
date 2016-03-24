7931d032458cca6bee0060090f6a93eabc4f2544
Ilija Hadzic <ilijahadzic@gmail.com>
2016-03-23 21:58:26 -0400

    web+html: add and hook up the token form
    Token form will be used by the administrator to generaete
    passwordless token-based access to the ladder and roster.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- jim/web.py
+++ jim/web.py
@@ -111,6 +111,10 @@ class SeasonFormHandler(GenericAdminFormHandler):
     def get(self):
         self.generic_get('season_form.html')
 
+class TokenFormHandler(GenericAdminFormHandler):
+    def get(self):
+        self.generic_get('token_form.html')
+
 class LoginHandler(DynamicBaseHandler):
     def get(self):
         self.log_request()
@@ -986,6 +990,7 @@ def run_server(ssl_options = util.test_ssl_options, http_port = 80, https_port =
         ('/match_form', MatchFormHandler),
         ('/player_form', PlayerFormHandler),
         ('/season_form', SeasonFormHandler),
+        ('/token_form', TokenFormHandler),
         ('/account_form', AccountFormHandler)
         ]
 
