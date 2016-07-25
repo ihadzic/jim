79180a763bfafc9e8c598453134fdb96ea662293
Adriaan van Wijngaarden <ajdlvw@gmail.com>
2016-07-25 04:51:23 -0400

    jim/web.py - addition of a phone number formatting routine/phone number check
    The function format_phone_number(phone_number) was defined to help check
    the input format, and to convert the input into a standard format, i.e.,
    xxx-xxx-xxxx. Note that this function can handle all kinds of strings
    with 10 digits, e.g., (908) 123 4567, or 908.123-4567. If the number
    does not contain 10 digits, it is rejected and None is returned. In
    one instance (home_phone), an if-statement is placed in the parser;
    for the cell phone and work phone number, the function format_phone_number
    is invoked. An error message is generated if a phone number was provided
    where the nunber of digits is not equal to 10. It is possible to further
    extend this to also accept international phone numbers, and/or to
    perform additional checks on the US phone numbers. One may also want
    to employ a package, e.g., python-phonenumbers.
    
    Signed-off-by: Adriaan van Wijngaarden <ajdlvw@gmail.com>

--- jim/web.py
+++ jim/web.py
@@ -473,7 +473,20 @@ class ReportHandler(InfoBaseHandler):
                     roster = roster,
                     season_string = season_string)
 
+
+def format_phone_number(phone_number):
+    p_dd = ''.join(c for c in phone_number if c.isdigit())
+    if (len(p_dd)==10):
+        ret_phone_number = '-'.join([p_dd[0:3],p_dd[3:6],p_dd[6:10]])
+    else: 
+        ret_phone_number = None
+    return ret_phone_number
+    
+
+
+
 class PlayerBaseHandler(DynamicBaseHandler):
+
     def parse_args(self, args, add_flag, password):
         try:
             username = args['username'][0]
@@ -503,16 +516,37 @@ class PlayerBaseHandler(DynamicBaseHandler):
             home_phone = args['home_phone'][0]
         except:
             home_phone = None
+
+        if (home_phone != None):
+            p_dd = ''.join(c for c in home_phone if c.isdigit())
+            if (len(p_dd)==10):
+                home_phone = '-'.join([p_dd[0:3],p_dd[3:6],p_dd[6:10]])
+            else: 
+                home_phone = None
+                self.finish_failure("A 10-digit home phone number is required.")
+                return None
+            
         try:
             cell_phone = args['cell_phone'][0]
         except:
             cell_phone = None
+        if (cell_phone != None):
+            cell_phone = format_phone_number(cell_phone)
+            if (cell_phone == None):
+               self.finish_failure("A 10-digit cell phone number is required.")
+               return None    
         try:
             work_phone = args['work_phone'][0]
         except:
             work_phone = None
+        if (work_phone != None):
+            work_phone = format_phone_number(work_phone)
+            if (work_phone == None):
+               self.finish_failure("A 10-digit work phone number is required.")
+               return None    
+            
         if add_flag and not (home_phone or cell_phone or work_phone):
-            self.finish_failure("at least one phone number is required")
+            self.finish_failure("At least one 10-digit phone number is required.")
             return None
         try:
             email = args['email'][0]
